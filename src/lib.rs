82a2db9a3fa0353877a7d1bb71b8c581929db009
Vujke <vladimirvuj@gmail.com>
2024-01-12 23:57:38 +0100

    add CF KV support

--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,15 @@
 mod error;
 mod utils;
+mod constants;
 
-use worker::{
-    console_error, console_log, event, Date, Env, Request, Response,
-    Result as WorkerResult, Router,
-};
+use std::str::FromStr;
+use worker::{console_error, console_log, event, Date, Env, Request, Response, Result as WorkerResult, Router, RouteContext};
 use rand::{ Rng };
 use rand::distributions::Alphanumeric;
 use serde::Deserialize;
 use serde_json::json;
+use crate::constants::KV_BINDING;
+use url::Url;
 use crate::error::Result;
 
 
@@ -27,7 +28,14 @@ fn log_request(req: &Request) {
     );
 }
 
-async fn handle_short_url_request(mut req: Request) -> Result<Response> {
+fn result_to_response(result: Result<Response>) -> Response {
+    result.unwrap_or_else(|e| {
+        console_error!("error: {:?}", e);
+        Response::from(e)
+    })
+}
+
+async fn handle_post_short_url_request(mut req: Request, ctx: RouteContext<()>) -> Result<Response> {
     let ShortUrlRequest { url } = req.json().await.map_err(|e| {
         console_log!("Error decoding JSON: {:?}", e);
         error::Error::InternalError("Invalid JSON in the request".to_string())
@@ -38,6 +46,9 @@ async fn handle_short_url_request(mut req: Request) -> Result<Response> {
         .take(7)
         .map(char::from)
         .collect();
+
+    let kv = ctx.kv(KV_BINDING)?;
+    kv.put(&shortlink, &url)?.execute().await?;
 
     console_log!("Received URL: {}, Received in URL: {}", url, req.url()?.host_str().unwrap());
 
@@ -50,12 +61,29 @@ async fn handle_short_url_request(mut req: Request) -> Result<Response> {
     Ok(resp)
 }
 
-fn result_to_response(result: Result<Response>) -> Response {
-    result.unwrap_or_else(|e| {
-        console_error!("error: {:?}", e);
-        Response::from(e)
-    })
+async fn handle_get_short_url_request(req: Request, ctx: RouteContext<()>) -> Result<Response> {
+    let key = req
+        .path()
+        .clone();
+
+    if let Some(p) = key.split("/").last() {
+        let kv = ctx.kv(KV_BINDING)?;
+        let actual_url = kv.get(p).text().await?;
+        if let Some(a) = actual_url {
+            let furl = Url::from_str(a.as_str())?;
+            let resp = Response::from_json(&json!({
+                "full_url": furl
+            }))?;
+            return Ok(resp);
+        } else {
+            Err(error::Error::InvalidRequest("Invalid short url.".to_string()))
+        }
+    } else {
+        Err(error::Error::InvalidRequest("Invalid path.".to_string()))
+    }
 }
+
+
 
 #[event(fetch)]
 async fn main(req: Request, env: Env, ctx: worker::Context) -> WorkerResult<Response> {
@@ -68,11 +96,12 @@ async fn main(req: Request, env: Env, ctx: worker::Context) -> WorkerResult<Resp
 
     return router
         .get("/", |_, _| { Response::ok("Hello from workers!!") })
-        .get("/:short_link", |_,_| {
-            Response::ok("")
+        .get_async("/:short_link", | req, ctx| async move {
+            let result = handle_get_short_url_request(req, ctx).await;
+            Ok(result_to_response(result))
         })
         .post_async("/get_short_url",  |req, ctx| async move {
-            let result = handle_short_url_request(req).await;
+            let result = handle_post_short_url_request(req, ctx).await;
             Ok(result_to_response(result))
         })
         .get("/worker-version", |_, ctx|{
